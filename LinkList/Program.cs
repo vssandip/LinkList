ea6c51485609526a2801725a662e3a0491297bd9
“Vssandip” <sandipshindesr95035@gmail.com>
2023-01-31 14:50:42 +0530

    [ SANDIP ] Adding  'SimpleLinkList'

--- LinkList/Program.cs
+++ LinkList/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace LinkList
+﻿using System.Collections.Generic;
+
+namespace LinkList
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("~~~ WELCOME TO LINKLIST ~~~");
+           SimpleLinkList a = new SimpleLinkList();
+            a.add(56);
+            a.add(30);
+            a.add(70);
+            a.show();
         }
     }
 }
