eb4fcf02854716ce1a1df5d735338f82f2a9f3bc
“Vssandip” <sandipshindesr95035@gmail.com>
2023-02-01 11:40:52 +0530

    [ SANDIP ] Adding  ' Ability to create Linked List by appending '

---
+++ LinkList/LinkedListAppending.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkList
+{
+    internal class LinkedListAppending
+    {
+        public Node head;
+
+        public void add(int data)
+        {
+            Node node = new Node();
+            node.data = data;
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+        }
+        public void AddFirst(int data)
+        {
+            Node node = new Node();
+            node.data = data;
+            node.next = head;
+            head = node;
+        }
+        public void show()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
--- LinkList/Program.cs
+++ LinkList/Program.cs
@@ -12,12 +12,16 @@ namespace LinkList
               a.add(30);
               a.add(70);
               a.show();*/
-            AddingLinkList a = new AddingLinkList();
+            /*AddingLinkList a = new AddingLinkList();
             a.add(56);
             a.add(30);
             a.add(70);
-            a.show();
-
+            a.show();*/
+            LinkedListAppending appending= new LinkedListAppending();
+            appending.add(70);
+            appending.AddFirst(30);
+            appending.AddFirst(56);
+            appending.show();
         }
     }
 }
